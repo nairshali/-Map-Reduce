d76a7a23f208b6f096d7bcc1635dca46bece1385
Shalini Nair <47002450+nairshali@users.noreply.github.com>
2019-05-15 21:58:30 +0100

    Correction of format validation

--- MapReduce/src/MapReduce.java
+++ MapReduce/src/MapReduce.java
@@ -10,25 +10,24 @@ public static void cleansing(String inputfile) throws IOException {
     	
 	    while ((strLine = br.readLine()) != null) {
 	    	String temp[] = strLine.split(",");
-            if ( temp[0].isEmpty() )// if row is empty
-            {
-            	System.out.println("ERROR : Empty Records ");
-            }
-            else if (temp[1].matches("^[A-Z]{3}\\d{4}\\[A-Z]{2}\\d{1}") == false ) {
-            	System.out.println("ERROR Passenger ID:");
-            }
-            else if (temp[2].matches("^[A-Z]{3}\\d{4}\\[A-Z]{1}") == false ) {
-            	System.out.println("ERROR Flight ID");
-            }
-            else if (temp[3].matches("^[A-Z]{3}") == false ) {
-            	System.out.println("From airport IATA/FAA code:");
-            }
-            else if (temp[1].matches("^[A-Z]{3}") == false ) {
-            	System.out.println("Destination airport IATA/FAA code:");
-            }
-            else {
-	    	      passengerDataFile.add(strLine);
-            }
+		 if ( temp[0].isEmpty() ){
+		     System.out.println("ERROR : Empty Records ");
+		  }
+		  else if (temp[0].matches("^[A-Z]{3}\\d{4}[A-Z]{2}\\d{1}") == false ) {
+		     System.out.println("ERROR Passenger ID:" + temp[0]);
+		  }
+		  else if (temp[1].matches("^[A-Z]{3}\\d{4}[A-Z]{1}") == false ) {
+		     System.out.println("ERROR Flight ID" + temp[1]);
+		  }
+		  else if (temp[2].matches("^[A-Z]{3}") == false ) {
+		     System.out.println("From airport IATA/FAA code:" + temp[2]);
+		  }
+		  else if (temp[3].matches("^[A-Z]{3}") == false ) {
+		     System.out.println("Destination airport IATA/FAA code:" + temp[3]);
+		  }
+		  else {
+		     passengerDataFile.add(strLine);
+		  }
 	    }    
 	    br.close(); 	
   	}
