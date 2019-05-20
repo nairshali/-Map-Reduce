80a72c3e01d805df9c75f3c752c56b5539bf80d1
Shalini Nair <47002450+nairshali@users.noreply.github.com>
2019-05-20 13:41:57 +0100

    Update passengerDetailsGui to build screen

--- MapReduce/src/FlightDetails.java
+++ MapReduce/src/FlightDetails.java
@@ -39,11 +39,62 @@ public class FlightDetails {
         gui.flightDetailsGui();
     }
   
-    public static void passengerDetailsGui(String[] args) throws IOException{ 
+    public static void passengerDetailsGui(String[] args) throws IOException{        
+		
 	JFrame passengerDetails = new JFrame();
 	passengerDetails.setBounds(100, 100, 450, 300);
 	passengerDetails.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    }
+		
+        JPanel passengerDetailsPanel = new JPanel();
+        passengerDetailsPanel.setBounds(0, 50, 434, 132);
+        passengerDetails.getContentPane().add(passengerDetailsPanel);
+        passengerDetailsPanel.setLayout(null);
+		
+	JLabel lblPassengerDetails = new JLabel("PASSENGER DETAILS");
+	lblPassengerDetails.setBounds(153, 11, 148, 14);
+	passengerDetailsPanel.add(lblPassengerDetails);
+		
+        // Table
+        final  JTable table = new JTable();
+        DefaultTableModel model = new DefaultTableModel();
+        
+	JScrollPane scrollPane = new JScrollPane(table);
+	scrollPane.setBounds(64, 36, 293, 58);
+	passengerDetailsPanel.add(scrollPane);
+        
+
+        intargs[0] = 3; // job ID
+        intargs[1] = 2; // Parallel Process
+        argsmap2[0] = new HashMap<String,Double>();
+        argsmap2[1] = new HashMap<String,Double>();
+        argsmap2[2] = new HashMap<String,Double>();
+	MapReduce.main(intargs,args,argsmap1,argsmap2); 
+		
+	model.addColumn("Passenger Id");
+	model.addColumn("Total Miles");
+		
+	for (String i : argsmap2[1].keySet()) {
+		System.out.println(i + " " + args[2]);
+		if (args[2].equals(i)) {
+			System.out.println(i + " " + argsmap2[1].get(i));
+				
+			Object[] data = {i,argsmap2[1].get(i)};
+			model.addRow(data);
+		}
+  	 }		
+		
+	table.setModel(model);
+	table.setPreferredScrollableViewportSize(new Dimension(500, 70));
+	table.setFillsViewportHeight(true);
+		
+	scrollPane.setVisible(true);
+	passengerDetailsPanel.add(scrollPane);
+		
+	passengerDetails.getContentPane().add(BorderLayout.CENTER,passengerDetailsPanel); 
+	passengerDetails.setSize(400,300);
+	passengerDetails.setVisible(true);
+
+	}
 	
     public void flightDetailsGui(){
 	JFrame flightDetails = new JFrame("Airline Flight Details");
